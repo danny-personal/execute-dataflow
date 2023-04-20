a5a1289d702261552bf13d26e443c48657bf175b
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-04-20 14:03:27 +0000

    Switching options

--- main.go
+++ main.go
@@ -10,20 +10,37 @@ import (
 	"google.golang.org/api/option"
 )
 
-func main() {
+func ExecuteDataflow() {
 	keyJson := os.Getenv("KEY_JSON")
 	projectID := os.Getenv("PROJECT_ID")
 	region := os.Getenv("DF_REGION")
 	fmt.Println(keyJson)
+
 	ctx := context.Background()
-	c, err := dataflow.NewTemplatesClient(ctx, option.WithCredentialsJSON([]byte(keyJson)))
+
+	tc, err := dataflow.NewTemplatesClient(ctx)
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	ctc, err := dataflow.NewTemplatesClient(ctx, option.WithCredentialsJSON([]byte(keyJson)))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var c *dataflow.TemplatesClient
+	if keyJson == "" {
+		c = tc
+		fmt.Println("That is tc.")
+	} else {
+		c = ctc
+		fmt.Println("That is ctc.")
+	}
 	defer c.Close()
+
 	req := &dataflowpb.CreateJobFromTemplateRequest{
 		ProjectId: projectID,
-		JobName:   "test3",
+		JobName:   "test10",
 		Location:  region,
 		Template: &dataflowpb.CreateJobFromTemplateRequest_GcsPath{
 			GcsPath: "gs://dataflow-templates-us-central1/latest/Word_Count",
@@ -34,10 +51,15 @@ func main() {
 		},
 		Environment: &dataflowpb.RuntimeEnvironment{TempLocation: "gs://danny-df-test/temp"},
 	}
+
 	res, err := c.CreateJobFromTemplate(ctx, req)
 	if err != nil {
 		fmt.Println(err)
 	}
 	_ = res
 	fmt.Println("end")
+}
+
+func main() {
+	ExecuteDataflow()
 }
